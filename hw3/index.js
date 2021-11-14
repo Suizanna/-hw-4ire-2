9879143fbc6388aa24b7ec164226fbe1c9386939
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-11-14 22:42:01 +0200

    add

--- hw3/index.js
+++ hw3/index.js
@@ -27,4 +27,40 @@ function filterBy(arr, type) {
 }
 
 const array = filterBy(['hello', 'world', 23, '23', null], 'string');
+console.group("#STRING IS FILTRED");
  console.log('array', array);
+ console.groupEnd("#STRING IS FILTRED");
+
+
+//3 вариант
+const array3 = ["hello", "world", 23, "23", null, undefined];
+
+function filterBy(array, type) {
+  const allTypes = [
+    "string",
+    "boolean",
+    "number",
+    undefined,
+    "object",
+    "bigint",
+    null,
+  ];
+
+  if (!allTypes.includes(type)) {
+    return `${type} такого типа данных не существует!`;
+  }
+  if (!Array.isArray(array)) {
+    return `${array} не массив, мы не можем работать дальше!`;
+  }
+  if (type === null || type === undefined) {
+    return array.filter((element) => element !== type);
+  }
+  return array.filter((element) => typeof element !== type);
+}
+
+console.log(array3);
+console.log("filterBy string", filterBy(array3, "string"));
+console.log(filterBy(array3, "str"));
+console.log("filterBy null", filterBy(array3, null));
+console.log(filterBy("string", "string"));
+console.log("filterBy undefined", filterBy(array3, undefined));
